48f4650734a1d7ebbe41e3d27f90f1b8cc0ae478
elenaburdin <66010914+elenaburdin@users.noreply.github.com>
2023-03-06 13:29:31 +0100

    Add files via upload

---
+++ Rectangle Area/script.js
@@ -0,0 +1,8 @@
+function rectangleArea(width, height) {
+    const rect = width * height;
+    console.log(`The width ${width} and height ${height} of the rectangle area is ${rect}`);
+}
+
+rectangleArea(2, 2);
+rectangleArea(10, 20);
+rectangleArea(30, 30);
