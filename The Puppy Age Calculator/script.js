a302da5e081588dbc6cf1ec9b5eb693e7def6a2b
elenaburdin <66010914+elenaburdin@users.noreply.github.com>
2023-03-06 12:24:25 +0100

    Add files via upload

---
+++ The Puppy Age Calculator/script.js
@@ -0,0 +1,11 @@
+function calculateDogAge(age) {
+    let dogAge = 7 * age;
+    return `Your doggie is ${dogAge} years old in dog years!`
+}
+
+
+
+console.log(calculateDogAge(1));
+console.log(calculateDogAge(8));
+console.log(calculateDogAge(5));
+
