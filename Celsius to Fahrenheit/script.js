1f89c3b2d12a85ddad10cf93369c9baa4dc52ebb
elenaburdin <66010914+elenaburdin@users.noreply.github.com>
2023-03-06 13:24:18 +0100

    Add files via upload

---
+++ Celsius to Fahrenheit/script.js
@@ -0,0 +1,10 @@
+function callFahrenheit(celsius) {
+    return fahrenheit = (celsius * 9 / 5) + 32;
+}
+
+console.log(callFahrenheit(8));
+console.log(callFahrenheit(10));
+console.log(callFahrenheit(5));
+console.log(callFahrenheit(0));
+console.log(callFahrenheit(20));
+console.log(callFahrenheit(40));
